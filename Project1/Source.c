987e4f164091245d6c573d830ce6a11d3a78bb41
Sevvoo <111128831+Sevvoo@users.noreply.github.com>
2023-07-06 23:36:12 +0300

    Update Source.c

--- Project1/Source.c
+++ Project1/Source.c
@@ -1,3 +1,4 @@
+//lab1
 #include <stdio.h>
 
 int main() {
@@ -12,6 +13,28 @@ int main() {
     double total_time = 1.0 / (1.0 / t1 + 1.0 / t2 + 1.0 / t3);
 
     printf("The time required for the guests to eat the cake: %.2f hours\n", total_time);
+
+    return 0;
+}
+
+
+//lab4
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a natural number: ");
+    scanf("%d", &n);
+
+    int count = 0; // counter for equal divisors
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == n / i) { // checking the quotient and remainder of division
+            count++;
+        }
+    }
+
+    printf("The number of equal divisors of %d is %d\n", n, count);
 
     return 0;
 }
